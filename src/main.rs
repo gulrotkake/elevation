2bab4c5f66de3a424e49ff412324a2e0505c8e04
Michael Mortensen <michael@f3k.org>
2024-04-21 19:50:26 +0200

    Cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
+use clap::Parser;
 use gdal::Dataset;
 use gdal::GeoTransformEx;
-use utm::to_utm_wgs84;
-use clap::Parser;
-use geojson::{GeoJson, FeatureCollection, Value};
 use geo::{point, HaversineDistance};
+use geojson::{FeatureCollection, GeoJson, Value};
 use poloto::build;
 use tagu::prelude::*;
+use utm::to_utm_wgs84;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -25,7 +25,10 @@ fn to_svg(data: &Vec<(f64, f64)>) {
     poloto::frame()
         .with_tick_lines([true, true])
         .build()
-        .data(poloto::plots!(poloto::build::origin(), build::plot("Profile").line(data)))
+        .data(poloto::plots!(
+            poloto::build::origin(),
+            build::plot("Profile").line(data)
+        ))
         .build_and_label(("Elevation", "Distance (meters)", "Altitude (meters)"))
         .append_to(poloto::header().append(theme))
         .render_stdout();
@@ -34,7 +37,13 @@ fn to_svg(data: &Vec<(f64, f64)>) {
 fn main() {
     let args = Args::parse();
     let ds = Dataset::open(&args.filename).unwrap();
-    eprintln!("Loading file {} {} {} bands: {}", args.filename, ds.driver().long_name(), ds.spatial_ref().unwrap().name().unwrap(), ds.raster_count());
+    eprintln!(
+        "Loading file {} {} {} bands: {}",
+        args.filename,
+        ds.driver().long_name(),
+        ds.spatial_ref().unwrap().name().unwrap(),
+        ds.raster_count()
+    );
     let band = ds.rasterband(1).unwrap();
 
     let transform = ds.geo_transform().unwrap();
@@ -54,20 +63,28 @@ fn main() {
                 _ => panic!("Expected a LineString"),
             };
 
-            let mut result: Vec<(f64, f64)> = coordinates.iter().enumerate().map(|(index, coord)| {
-                let (lng, lat) = (coord[0], coord[1]);
-                distance += if index > 0 {
-                    let p1 = point!(x: coord[0], y: coord[1]);
-                    let prev = &coordinates[index - 1];
-                    let p0 = point!(x: prev[0], y: prev[1]);
-                    p1.haversine_distance(&p0)
-                } else { 0.0 };
+            let mut result: Vec<(f64, f64)> = coordinates
+                .iter()
+                .enumerate()
+                .map(|(index, coord)| {
+                    let (lng, lat) = (coord[0], coord[1]);
+                    distance += if index > 0 {
+                        let p1 = point!(x: coord[0], y: coord[1]);
+                        let prev = &coordinates[index - 1];
+                        let p0 = point!(x: prev[0], y: prev[1]);
+                        p1.haversine_distance(&p0)
+                    } else {
+                        0.0
+                    };
 
-                let (north, east, _) = to_utm_wgs84(lat, lng, 33);
-                let (tx, ty) = inv_transform.apply(east, north);
-                let buf = band.read_as::<f64>((tx as isize, ty as isize), (1, 1), (1,1), None).unwrap();
-                (distance.round(), buf.data[0].round())
-            }).collect();
+                    let (north, east, _) = to_utm_wgs84(lat, lng, 33);
+                    let (tx, ty) = inv_transform.apply(east, north);
+                    let buf = band
+                        .read_as::<f64>((tx as isize, ty as isize), (1, 1), (1, 1), None)
+                        .unwrap();
+                    (distance.round(), buf.data[0].round())
+                })
+                .collect();
             total.append(&mut result);
         }
     }
